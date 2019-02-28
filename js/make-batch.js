ab6b31432d002cecc19ee80a870dff49f56872d7
Raku Hernandez <raul.r.h.rosas@gmail.com>
2019-02-28 13:50:45 -0800

    Added some tests for list, and fixed list functionality.

--- js/make-batch.js
+++ js/make-batch.js
@@ -1,68 +1,71 @@
 'use strict';
 
 angular.module('myApp.makeBatch', ['ngRoute']).
-    config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/make-batch', {
-            templateUrl: 'views/make-batch.html',
-            controller: 'MakeBatchCtrl'
-        });
-    }])
+config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/make-batch', {
+        templateUrl: 'views/make-batch.html',
+        controller: 'MakeBatchCtrl'
+    });
+}])
 
-    .controller('MakeBatchCtrl', ['$scope', '$http', function ($scope, $http) {
+.controller('MakeBatchCtrl', ['$scope', '$http', function($scope, $http) {
 
-        var recipe_list;
-        var poolData = {
-            UserPoolId: _config.cognito.userPoolId,
-            ClientId: _config.cognito.userPoolClientId
-        };
-        var userPool;
-        userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+    var recipe_list;
+    var poolData = {
+        UserPoolId: _config.cognito.userPoolId,
+        ClientId: _config.cognito.userPoolClientId
+    };
+    var userPool;
+    userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
 
-        var authToken;
-        window.authToken.then(function setAuthToken(token) {
-            if (token) {
-                getrecipeList(token);
-            } else {
-                window.location.href = '#!/login';
-            }
-        }).catch(function handleTokenError(error) {
-            alert(error);
+    var authToken;
+    window.authToken.then(function setAuthToken(token) {
+        if (token) {
+            getrecipeList(token);
+        } else {
             window.location.href = '#!/login';
+        }
+    }).catch(function handleTokenError(error) {
+        alert(error);
+        window.location.href = '#!/login';
+    });
+
+
+    function getrecipeList(token) {
+        var req = {
+            method: 'POST',
+            url: _config.api.invokeUrl + '/getrecipe',
+            headers: {
+                Authorization: token
+            },
+            data: { test: 'test' }
+        }
+        $http(req).then(function successCallback(response) {
+            console.log('Success');
+            recipe_list = response.data.Items;
+            $scope.recipes = recipe_list;
+        }, function errorCallback(response) {
+            console.error('Error');
+
         });
+    }
 
+    $scope.orderByMe = function(x) {
+        $scope.myOrderBy = x;
+    }
 
-        function getrecipeList(token) {
-            var req = {
-                method: 'POST',
-                url: _config.api.invokeUrl + '/getrecipe',
-                headers: {
-                    Authorization: token
-                },
-                data: { test: 'test' }
-            }
-            $http(req).then(function successCallback(response) {
-                console.log('Success');
-                recipe_list = response.data.Items;
-                $scope.recipes = recipe_list;
-            }, function errorCallback(response) {
-                console.error('Error');
+    $scope.dateConvert = function(timestamp) {
 
-            });
-        }
+        var months_arr = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+        var date = new Date(timestamp * 1000);
+        var year = date.getFullYear();
+        var month = months_arr[date.getMonth()];
+        var day = date.getDate();
+        var convdataTime = month + '-' + day + '-' + year;
 
-        $scope.orderByMe = function(x){
-            $scope.myOrderBy = x;
-        }
-
-        $scope.dateConvert = function(timestamp) {
-
-            var months_arr = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-            var date = new Date(timestamp * 1000);
-            var year = date.getFullYear();
-            var month = months_arr[date.getMonth()];
-            var day = date.getDate();
-            var convdataTime = month + '-' + day + '-' + year;
-
-            return (convdataTime);
-        }
-    }]);
+        return (convdataTime);
+    }
+    $scope.test = function(name) {
+        alert(name);
+    }
+}]);
