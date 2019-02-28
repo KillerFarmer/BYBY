d352aba7a6e028c052eeda4e4e824fa7e44be86d
superpolster <alex.superpolster@gmail.com>
2019-02-27 21:15:51 -0800

    Lee la respuesta del servidor y la despliega en una lista.

--- js/make-batch.js
+++ js/make-batch.js
@@ -42,10 +42,27 @@ config(['$routeProvider', function($routeProvider) {
         $http(req).then(function successCallback(response) {
             console.log('Success');
             recipe_list = response.data.Items;
+            var x = " ";
             //Aqui pon la primera vez que se organiza esque es asincrono entonces le vale el tiempo, andrade.
+            for (i in recipe_list) {
+                x += "<li class='collection-item deep-purple accent-1'><div>" + recipe_list.Name + "<a class='secondary-content black-text'><i>dateConvert" + dateConvert(recipe_list.Timestamp) + "</i></a></div></li><br>";
+              }
+            document.getElementById("rlist").innerHTML = x;
         }, function errorCallback(response) {
             console.error('Error');
 
         });
     }
+
+    function dateConvert(timestamp){
+       
+        var months_arr = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+        var date = new Date(timestamp*1000);
+        var year = date.getFullYear();
+        var month = months_arr[date.getMonth()];
+        var day = date.getDate();
+        var convdataTime = month+'-'+day+'-'+year;
+
+        return(convdataTime);
+       }
 }]);
