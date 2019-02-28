057b039db3ed4daedf01ab6ddf15af9d3e4b739e
Raku Hernandez <raul.r.h.rosas@gmail.com>
2019-02-27 16:19:49 -0800

    Changes to make-batch.js
    Made changes to invoke url and make-batch.js

--- js/make-batch.js
+++ js/make-batch.js
@@ -1,60 +1,48 @@
 'use strict';
 
 angular.module('myApp.makeBatch', ['ngRoute']).
-    config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/make-batch', {
-            templateUrl: 'views/make-batch.html',
-            controller: 'MakeBatchCtrl'
-        });
-    }])
+config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/make-batch', {
+        templateUrl: 'views/make-batch.html',
+        controller: 'MakeBatchCtrl'
+    });
+}])
 
-    .controller('MakeBatchCtrl', ['$scope', '$http', function ($scope, $http) {
-        var recipe_list;
-        var poolData = {
-            UserPoolId: _config.cognito.userPoolId,
-            ClientId: _config.cognito.userPoolClientId
-        };
-        var userPool;
-        userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+.controller('MakeBatchCtrl', ['$scope', '$http', function($scope, $http) {
+    var recipe_list;
+    var poolData = {
+        UserPoolId: _config.cognito.userPoolId,
+        ClientId: _config.cognito.userPoolClientId
+    };
+    var userPool;
+    userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
 
-        var authToken;
-        window.authToken.then(function setAuthToken(token) {
-            if (token) {
-                authToken = token;
-            } else {
-                window.location.href = '#!/login';
-            }
-        }).catch(function handleTokenError(error) {
-            alert(error);
+    var authToken;
+    window.authToken.then(function setAuthToken(token) {
+        if (token) {
+            getrecipeList(token);
+        } else {
             window.location.href = '#!/login';
-        });
-        window.authToken.then(function updateAuthMessage(token) {
-            authToken = token;
-        });
+        }
+    }).catch(function handleTokenError(error) {
+        alert(error);
+        window.location.href = '#!/login';
+    });
+
+    function getrecipeList(token) {
         var req = {
             method: 'POST',
             url: _config.api.invokeUrl + '/getrecipe',
-
             headers: {
-                Authorization: authToken,
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "*",
-                "Access-Control-Allow-Credentials": true,
-                "Access-Control-Allow-Headers": "*",
+                Authorization: token,
+                withCredentials: true,
             },
-            data: {
-
-            },
-            contentType: undefined,
-            cors: true,
+            data: { test: 'test' }
         }
-        $http(req).then(function mySuccess(response) {
-            console.log('Succes');
-        }, function myError(response) {
-            console.log('Error');
+        $http(req).then(function successCallback(response) {
+            console.log('Success');
+        }, function errorCallback(response) {
+            console.error('Error: ' + JSON.stringify(response));
         });
-
-        function orderListABC(data) {
-            alert(data);//response
-        }
-    }]);
+    }
+}]);
